ecc1bc08e94301017ebced8c10ba2d5bbc68bb8c
aehayes99 <60195801+aehayes99@users.noreply.github.com>
2020-03-04 19:15:55 -0500

    story

---
+++ story copy/story.js
@@ -0,0 +1,10 @@
+$("#move").mouseenter(function () {
+
+    $(this).animate({
+        top: Math.random() * 300
+    }, 100);
+    $(this).animate({
+        left: Math.random() * 300
+    }, 100);
+
+});
